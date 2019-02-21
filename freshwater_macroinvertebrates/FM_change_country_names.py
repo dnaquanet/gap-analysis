694e983521ed296b53e99a8384f9bc70972a6a34
haweigand <haweigand@users.noreply.github.com>
2019-02-21 14:34:42 +0100

    Create FM_change_country_names.py

---
+++ freshwater_macroinvertebrates/FM_change_country_names.py
@@ -0,0 +1,40 @@
+import os
+
+def load_country_names(file1):
+    cou_names = {}
+    for line in open(file1):
+        line2 = line.strip("\n").split("\t")
+        line2 = [l.strip(" ") for l in line2]
+        cou_names[line2[0]] = line2[1]
+        cou_names[line2[1]] = line2[1]
+    return(cou_names)
+
+def load_files(folder):
+    files = os.listdir(folder)
+    return(files)
+
+def make_new_file(cou_names,folder,foldern,file1):
+    l = 0
+    out = open(foldern + "/" + file1, "w")
+    for line in open(folder + "/" + file1):
+        line2 = line.rstrip("\n").replace("?","").split("\t")
+        l += 1
+        if l == 1:
+            out.write("\t".join(line2))
+        else:
+            cou = line2[5].split(";")
+            coun = [cou_names.get(c,"NO") for c in cou]
+            if "NO" in coun:
+                print(line2)
+                print(coun)
+            else:
+                coun = ";".join(sorted(coun))
+                out.write("\n" + "\t".join(line2[0:5]) + "\t" + coun)
+    out.close()
+
+
+cou_names = load_country_names("country_names.txt")
+files = load_files("summary")
+for f in files:
+    print(f)
+    make_new_file(cou_names,"summary","summary_new_names",f)
